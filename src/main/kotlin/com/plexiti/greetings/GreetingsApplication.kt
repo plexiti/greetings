a68d80aa6e921c8cdc9b7dc899dae61249f00975
Martin Schimak <martin@halvar.local>
2017-05-31 19:09:52 +0200

    Minor improvements

--- src/main/kotlin/com/plexiti/greetings/GreetingsApplication.kt
+++ src/main/kotlin/com/plexiti/greetings/GreetingsApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application
+class GreetingsApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    SpringApplication.run(GreetingsApplication::class.java, *args)
 }
-
