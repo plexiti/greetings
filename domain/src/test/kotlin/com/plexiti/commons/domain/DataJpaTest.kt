b7cb21e666dfec23bd141439d81356de10de7681
Martin Schimak <martin.schimak@plexiti.com>
2017-06-19 09:02:26 +0200

    Add Spring Data/JPA Test Configuration and Template Class

---
+++ domain/src/test/kotlin/com/plexiti/commons/domain/DataJpaTest.kt
@@ -0,0 +1,29 @@
+package com.plexiti.commons.domain
+
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+@RunWith(SpringRunner::class)
+@ContextConfiguration
+@DataJpaTest
+class DataJpaTest {
+
+    @Autowired
+    lateinit var eventRepository: EventEntityRepository
+
+
+    @Configuration
+    @EntityScan("com.plexiti.commons")
+    @EnableJpaRepositories("com.plexiti.commons")
+    class DataJpaTestConfiguration
+
+}
